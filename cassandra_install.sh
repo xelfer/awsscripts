9974ac1a1f96253b5f3971a87ac0b4c1dff8eff7
Nick Triantafillou <nicolast@gmail.com>
2014-07-15 15:25:04 +1000

    full path updates

--- cassandra_install.sh
+++ cassandra_install.sh
@@ -1,11 +1,8 @@
-cd
-CASSANDRA_VERSION=2.0.9
-apt-get update && apt-get install openjdk-7-jdk -y
-wget wget http://mirror.mel.bkb.net.au/pub/apache/cassandra/2.0.9/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
-tar zxvf apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
-mv apache-cassandra-$CASSANDRA_VERSION cassandra
-export CASSANDRA_HOME=~/cassandra
-export PATH=$PATH:$CASSANDRA_HOME/bin
-sh ~/cassandra/bin/cassandra
-
-echo "Install complete. Run ~/cassandra/bin/cassandra-cli to connect."
+#!
+export CASSANDRA_VERSION=2.0.9
+/usr/bin/apt-get update && /usr/bin/apt-get install openjdk-7-jdk -y
+/usr/bin/wget http://mirror.mel.bkb.net.au/pub/apache/cassandra/2.0.9/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz -O /root/cassandra.tar.gz 
+mkdir /root/cassandra
+tar zxf /root/cassandra.tar.gz -C /root/cassandra --strip-components=1
+echo "export CASSANDRA_HOME=~/cassandra" >> /root/.bashrc
+echo "export PATH=$PATH:$CASSANDRA_HOME/bin" >> /root.bashrc
