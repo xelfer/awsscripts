c2bf3423ff5ccb822548134531860bb8d5099af0
Nick Triantafillou <nicolast@gmail.com>
2014-07-15 13:52:45 +1000

    add variable for cassandra version

--- cassandra_install.sh
+++ cassandra_install.sh
@@ -1,8 +1,9 @@
 cd
-apt-get install openjdk-7-jdk
-wget wget http://mirror.mel.bkb.net.au/pub/apache/cassandra/2.0.9/apache-cassandra-2.0.9-bin.tar.gz
-tar zxvf apache-cassandra*
-mv apache-cassandra* cassandra
+CASSANDRA_VERSION=2.0.9
+apt-get update && apt-get install openjdk-7-jdk -y
+wget wget http://mirror.mel.bkb.net.au/pub/apache/cassandra/2.0.9/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
+tar zxvf apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
+mv apache-cassandra-$CASSANDRA_VERSION cassandra
 export CASSANDRA_HOME=~/cassandra
 export PATH=$PATH:$CASSANDRA_HOME/bin
 sh ~/cassandra/bin/cassandra
